61ee969aae744889a2fdbc837afcbfd3ad4a68cf
Mattis <m.erkensten@icloud.com>
2021-09-29 16:34:04 +0200

    Funktionalitet för logga in sidan påbörjad

--- src/view/SignInView.jsx
+++ src/view/SignInView.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const SignInView = () => {
-    return (
-        <div>
-            <h1>Detta är logga in vyn</h1>
-        </div>
-    )
-}
+  const [loggedInUser, setLoggedInUser] = useState();
+  const [passoword, setPassword] = useState();
+
+  return (
+    <div>
+      <div className="username">
+          <p>Användarnamn: </p>
+          <input onChange={(event) => setLoggedInUser(event.target.value)}  type="text" placeholder="Skriv in ett användarnamn" />
+      </div>
+      <div className="password">
+          <p>Lösenord: </p>
+          <input type="password" onChange={(event) => setPassword(event.target.value)} placeholder="Skriv in ditt lösenord..."/>
+      </div>
+      <button>Logga in</button>
+    </div>
+  );
+};
